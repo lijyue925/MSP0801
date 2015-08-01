80d12e15c994f1dfb263dfc5a4a503d06f50467b
張守群 <j245700@livemail.tw>
2015-08-01 13:34:18 +0800

    data binding

---
+++ MSP0801/ResultConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace MSP0801
+{
+    class ResultConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            double result = (double) value;
+            if (result < 20)
+            {
+                return new SolidColorBrush(Colors.ForestGreen);
+            }
+            else if (result > 25)
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.Blue);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
